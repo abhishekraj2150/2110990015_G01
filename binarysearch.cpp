d64bfd3a227c3ce04609abe3c6ad150bd00d09a0
aanchalsharmaa1024 <aanchal0009.be21@chitkara.edu.in>
2022-05-31 22:35:22 +0530

    search func added

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 int main()
 {
-
+bool search(int arr[],int size,int key){
+    for(int i=0; i<size; i++){
+        if(arr[i]==key){
+            return 1;
+        }
+    }
+    return 0;
+}
 
 
 
